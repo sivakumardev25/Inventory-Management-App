b68f4b3f9687da9e62e5e7150d33dced961d2fdc
sivakumardev25 <sivakumarr2000@gmail.com>
2026-07-14 23:14:08 +0530

    Updating api

--- frontend/inventory-fe/src/utils/api.js
+++ frontend/inventory-fe/src/utils/api.js
@@ -1,20 +1,17 @@
 import axios from "axios";
 
-const rawApiBaseUrl =
-  process.env.REACT_APP_API_BASE_URL ||
-  process.env.REACT_APP_BASE_API_URL ||
-  "/api";
-
-const apiBaseUrl = rawApiBaseUrl.replace(/\/auth\/?$/, "").replace(/\/$/, "");
-
 const api = axios.create({
-  baseURL: apiBaseUrl,
+  baseURL:
+    process.env.REACT_APP_BASE_API_URL ||
+    process.env.REACT_APP_API_BASE_URL ||
+    "/api",
   headers: {
     "Content-Type": "application/json",
   },
   // withCredentials: true,
   timeout: 15000, // 10 seconds timeout
 });
+// console.log("API Base URL:", baseURL);
 
 api.interceptors.response.use(
   (res) => res,
@@ -26,3 +23,6 @@ api.interceptors.response.use(
 );
 
 export default api;
+
+  
+
