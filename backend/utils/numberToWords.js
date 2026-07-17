91b0d3954178d2f4f45af4e86221b0dc0fe7841f
sivakumardev25 <sivakumarr2000@gmail.com>
2026-07-17 10:20:14 +0530

    fix correct spacing in numberToWords amount formatting

--- backend/utils/numberToWords.js
+++ backend/utils/numberToWords.js
@@ -7,13 +7,12 @@ const tens = [
     "", "", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"];
   
 function b100(num) {
-    return num < 20 ? ones[num] : tens[Math.floor(num / 10)] + (num % 10 ? "" + ones[num % 10] : "");
+    return num < 20 ? ones[num] : tens[Math.floor(num / 10)] + (num % 10 ? " " + ones[num % 10] : "");
 }
 
 function b1000(num) {
     return num < 100 ? b100(num) : ones[Math.floor(num / 100)] + " Hundred" + (num % 100 ? " " + b100(num % 100) : "");
 }
-
 
 function numberToWords(amount) {
     const n = Math.round(Number(amount));
@@ -22,16 +21,16 @@ function numberToWords(amount) {
 
     let w = '';
     if (n >= 10000000)
-        w += b1000(Math.floor(n / 10000000)) + 'Crore';
+        w += b1000(Math.floor(n / 10000000)) + ' Crore';
     if (Math.floor(n / 100000) % 100)
-        w += ' ' + b100(Math.floor(n / 100000) % 100) + 'Lakh';
+        w += ' ' + b100(Math.floor(n / 100000) % 100) + ' Lakh';
     if (Math.floor(n / 1000) % 100) 
-        w += ' ' + b100(Math.floor(n / 1000) % 100) + 'Thousand';
+        w += ' ' + b100(Math.floor(n / 1000) % 100) + ' Thousand';
     if (Math.floor(n/100) % 10) 
-        w += ' ' + ones[Math.floor(n / 100) % 10] + 'Hundred';
+        w += ' ' + ones[Math.floor(n / 100) % 10] + ' Hundred';
     if (n % 100)
         w += ' ' + b100(n % 100);
-    return w.trim() + 'Rupees Only';
+    return w.trim() + ' Rupees Only';
 }
 
 module.exports = { numberToWords };
